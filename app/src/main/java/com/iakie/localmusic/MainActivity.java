c1c5f86383222da2bfe69638fed7ccb0aabbfc38
iqiqiya <43666095+iqiqiya@users.noreply.github.com>
2019-11-29 18:11:42 +0800

    数据展示完毕

--- app/src/main/java/com/iakie/localmusic/MainActivity.java
+++ app/src/main/java/com/iakie/localmusic/MainActivity.java
@@ -1,16 +1,112 @@
 package com.iakie.localmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView nextIv,playIv,lastIv;
+    TextView singerTv,songTv;
+    RecyclerView musicRv;
+
+    // 数据源
+    List<LocalMusicBean>mDatas;
+    private LocalMusicAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        mDatas = new ArrayList<>();
+
+        // 创建适配器对象
+        adapter = new LocalMusicAdapter(this, mDatas);
+        musicRv.setAdapter(adapter);
+
+        // 设置布局管理器  竖向线性布局  false不反转
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        musicRv.setLayoutManager(layoutManager);
+
+        // 加载本地数据源
+        loadLocalMusicData();
     }
 
+    private void loadLocalMusicData() {
+        // 加载本地存储中的音乐mp3文件到集合当中
+        // 1.获取ContentResolver对象
+        ContentResolver resolver = getContentResolver();
+        // 2.获取本地音乐存储的Uri地址
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        // 3.开始查询地址
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        // 4.遍历Cursor
+        int id = 0;
+        while (cursor.moveToNext()) {
+            String song = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String singer = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+            String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+            id++;
+            String sid = String.valueOf(id);
+            String path = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+            long duraation = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
+            SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+            String time =  sdf.format(new Date(duraation));
+            // 将一行中的数据封装到对象当中
+            LocalMusicBean bean = new LocalMusicBean(sid,song,singer,album,time,path);
+            mDatas.add(bean);
+        }
 
+        // 数据发生变化，提示适配器更新
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        /*初始化控件*/
+        nextIv = findViewById(R.id.local_music_bottom_iv_next);
+        playIv = findViewById(R.id.local_music_bottom_iv_play);
+        lastIv = findViewById(R.id.local_music_bottom_iv_last);
+        singerTv = findViewById(R.id.local_music_bottom_tv_singer);
+        songTv = findViewById(R.id.local_music_bottom_tv_song);
+        musicRv = findViewById(R.id.local_music_rv);
+
+        nextIv.setOnClickListener(this);
+        lastIv.setOnClickListener(this);
+        playIv.setOnClickListener(this);
+    }
+
+    // 三个控制按钮的点击事件
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.local_music_bottom_iv_last:
+                // TODO
+                break;
+            case R.id.local_music_bottom_iv_next:
+                // TODO
+            case R.id.local_music_bottom_iv_play:
+                // TODO
+                break;
+        }
+    }
 }
